9268cc82c91f27e0baccff6cccd8773bebf610dd
Higor Prado <higorevop@gmail.com>
2023-03-25 15:09:47 -0300

    Add more tests to deal with date format and other things

--- tests/test_api.py
+++ tests/test_api.py
@@ -46,3 +46,31 @@ def test_upload_non_csv_file():
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
     assert response.json() == {"detail": "Invalid file format. Please upload a CSV file."}
+
+def test_upload_csv_with_negative_values():
+    csv_data = "review_time,team,date,merge_time\n0,Application,2023-01-14,0\n-1,Application,2023-01-15,0\n715,Application,2023-01-16,-597"
+    files = {"file": ("negative_values.csv", csv_data, "text/csv")}
+    response = client.post("/upload", files=files)
+    assert response.status_code == 400, f"Response: {response.text}"
+    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values."}
+
+def test_upload_csv_with_additional_fields():
+    csv_data = "review_time,team,date,merge_time,extra_field\n0,Application,2023-01-14,0,100\n0,Application,2023-01-15,0,200\n715,Application,2023-01-16,597,300"
+    files = {"file": ("additional_fields.csv", csv_data, "text/csv")}
+    response = client.post("/upload", files=files)
+    assert response.status_code == 201, f"Response: {response.text}"
+    assert response.json() == {"detail": "Data uploaded successfully"}
+
+def test_upload_csv_with_missing_fields():
+    csv_data = "review_time,date,merge_time\n0,2023-01-14,0\n0,2023-01-15,0\n715,2023-01-16,597"
+    files = {"file": ("missing_fields.csv", csv_data, "text/csv")}
+    response = client.post("/upload", files=files)
+    assert response.status_code == 400, f"Response: {response.text}"
+    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are present in each row."}
+
+def test_upload_csv_with_invalid_date_format():
+    csv_data = "review_time,team,date,merge_time\n0,Application,14/01/2023,0\n0,Application,15/01/2023,0\n715,Application,16/01/2023,597"
+    files = {"file": ("invalid_date_format.csv", csv_data, "text/csv")}
+    response = client.post("/upload", files=files)
+    assert response.status_code == 400, f"Response: {response.text}"
+    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values."}
