c9a5eab48584a4a88ba7c7154a486bba2db244b8
Higor Prado <higorevop@gmail.com>
2023-03-25 17:16:33 -0300

    Refactor the code including type hinting

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import io
+import datetime
+from typing import List, Union, Dict
+
 import pandas as pd
 from fastapi import FastAPI, File, UploadFile, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+
 from database import SessionLocal, engine
 from models import AnalyticsData, Base
 
@@ -12,9 +16,20 @@ app = FastAPI()
 
 MAX_FILE_SIZE = 1024 * 1024 * 10  # 10 MB
 REQUIRED_FIELDS = ["review_time", "team", "date", "merge_time"]
+ERROR_MESSAGES: Dict[str, str] = {
+    "invalid_csv": "Invalid CSV file. Please make sure all required fields are present in each row.",
+    "invalid_date_format": "Invalid date format. Please make sure all date values are in the format YYYY-MM-DD.",
+    "invalid_null_values": "Invalid CSV file. Please make sure all required fields are non-null values.",
+    "invalid_integers": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values.",
+    "invalid_file_format": "Invalid file format. Please upload a CSV file.",
+    "file_too_large": "File too large. Please upload a smaller CSV file.",
+    "empty_file": "Empty file. Please upload a non-empty CSV file.",
+    "upload_successful": "Data uploaded successfully",
+    "processing_error": "Error occurred while processing the CSV file: {error}",
+}
 
 
-def get_db():
+def get_db() -> Session:
     db = SessionLocal()
     try:
         yield db
@@ -22,7 +37,7 @@ def get_db():
         db.close()
 
 
-async def read_file_content(file: UploadFile):
+async def read_file_content(file: UploadFile) -> Union[bytes, None]:
     file_size = 0
     file_content = b""
     while True:
@@ -36,24 +51,36 @@ async def read_file_content(file: UploadFile):
     return file_content
 
 
-def validate_and_parse_csv(file_content):
+def is_valid_date(date_str: str) -> bool:
+    try:
+        datetime.datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+
+def validate_and_parse_csv(file_content: bytes) -> Union[List[AnalyticsData], JSONResponse]:
     csv_file = io.StringIO(file_content.decode("utf-8"))
     df = pd.read_csv(csv_file)
 
     if not all(field in df.columns for field in REQUIRED_FIELDS):
         return JSONResponse(
             status_code=400,
-            content={
-                "detail": "Invalid CSV file. Please make sure all required fields are present in each row."
-            },
+            content={"detail": ERROR_MESSAGES["invalid_csv"]},
+        )
+
+    df = df[REQUIRED_FIELDS]
+
+    if not df["date"].apply(is_valid_date).all():
+        return JSONResponse(
+            status_code=400,
+            content={"detail": ERROR_MESSAGES["invalid_date_format"]},
         )
 
     if df.isnull().values.any():
         return JSONResponse(
             status_code=400,
-            content={
-                "detail": "Invalid CSV file. Please make sure all required fields are non-null values."
-            },
+            content={"detail": ERROR_MESSAGES["invalid_null_values"]},
         )
 
     df = df.astype({"review_time": "int32", "merge_time": "int32"})
@@ -61,14 +88,11 @@ def validate_and_parse_csv(file_content):
     if not df.apply(lambda x: x.review_time >= 0 and x.merge_time >= 0, axis=1).all():
         return JSONResponse(
             status_code=400,
-            content={
-                "detail": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values."
-            },
+            content={"detail": ERROR_MESSAGES["invalid_integers"]},
         )
 
     data = [AnalyticsData(**row) for _, row in df.iterrows()]
     return data
-
 
 
 @app.post("/upload", status_code=201)
@@ -76,20 +100,20 @@ async def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)
     if file.content_type != "text/csv":
         return JSONResponse(
             status_code=400,
-            content={"detail": "Invalid file format. Please upload a CSV file."},
+            content={"detail": ERROR_MESSAGES["invalid_file_format"]},
         )
 
     file_content = await read_file_content(file)
     if file_content is None:
         return JSONResponse(
             status_code=400,
-            content={"detail": "File too large. Please upload a smaller CSV file."},
+            content={"detail": ERROR_MESSAGES["file_too_large"]},
         )
 
     if len(file_content) == 0:
         return JSONResponse(
             status_code=400,
-            content={"detail": "Empty file. Please upload a non-empty CSV file."},
+            content={"detail": ERROR_MESSAGES["empty_file"]},
         )
 
     data = validate_and_parse_csv(file_content)
@@ -99,11 +123,10 @@ async def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)
     try:
         db.add_all(data)
         db.commit()
-        return {"detail": "Data uploaded successfully"}
-
+        return {"detail": ERROR_MESSAGES["upload_successful"]}
     except Exception as e:
         db.rollback()
-        return JSONResponse(
-            status_code=500,
-            content={"detail": f"Error occurred while processing the CSV file: {e}"},
-        )
+    return JSONResponse(
+        status_code=500,
+        content={"detail": ERROR_MESSAGES["processing_error"].format(error=e)},
+    )
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,32 +5,47 @@ from main import app
 
 client = TestClient(app)
 
+ERROR_MESSAGES = {
+    "invalid_file_format": "Invalid file format. Please upload a CSV file.",
+    "empty_file": "Empty file. Please upload a non-empty CSV file.",
+    "invalid_csv": "Invalid CSV file. Please make sure all required fields are non-null values.",
+    "file_too_large": "File too large. Please upload a smaller CSV file.",
+    "invalid_values": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values.",
+    "missing_fields": "Invalid CSV file. Please make sure all required fields are present in each row.",
+    "invalid_date_format": "Invalid date format. Please make sure all date values are in the format YYYY-MM-DD.",
+    "upload_successful": "Data uploaded successfully",
+}
+
+
 @pytest.fixture(scope="module", autouse=True)
 def setup_and_teardown():
-    create_tables()  
+    create_tables()
     yield
     drop_tables()
+
 
 def test_upload_csv():
     csv_data = "review_time,team,date,merge_time\n0,Application,2023-01-14,0\n0,Application,2023-01-15,0\n715,Application,2023-01-16,597"
     files = {"file": ("sample.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 201, f"Response: {response.text}"
-    assert response.json() == {"detail": "Data uploaded successfully"}
+    assert response.json() == {"detail": ERROR_MESSAGES["upload_successful"]}
+
 
 def test_upload_empty_csv():
     csv_data = ""
     files = {"file": ("empty.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Empty file. Please upload a non-empty CSV file."}
+    assert response.json() == {"detail": ERROR_MESSAGES["empty_file"]}
+
 
 def test_upload_invalid_csv():
     csv_data = "review_time,team,date,merge_time\n0,Application,2023-01-14,0\n0,Application,2023-01-15\n715,Application,2023-01-16,597"
     files = {"file": ("invalid.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are non-null values."}
+    assert response.json() == {"detail": ERROR_MESSAGES["invalid_csv"]}
 
 
 def test_upload_large_csv():
@@ -38,39 +53,44 @@ def test_upload_large_csv():
     files = {"file": ("large.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "File too large. Please upload a smaller CSV file."}
+    assert response.json() == {"detail": ERROR_MESSAGES["file_too_large"]}
+
 
 def test_upload_non_csv_file():
     csv_data = "review_time,team,date,merge_time\n0,Application,2023-01-14,0\n0,Application,2023-01-15,0\n715,Application,2023-01-16,597"
     files = {"file": ("sample.txt", csv_data, "text/plain")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Invalid file format. Please upload a CSV file."}
+    assert response.json() == {"detail": ERROR_MESSAGES["invalid_file_format"]}
+
 
 def test_upload_csv_with_negative_values():
     csv_data = "review_time,team,date,merge_time\n0,Application,2023-01-14,0\n-1,Application,2023-01-15,0\n715,Application,2023-01-16,-597"
     files = {"file": ("negative_values.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values."}
+    assert response.json() == {"detail": ERROR_MESSAGES["invalid_values"]}
+
 
 def test_upload_csv_with_additional_fields():
     csv_data = "review_time,team,date,merge_time,extra_field\n0,Application,2023-01-14,0,100\n0,Application,2023-01-15,0,200\n715,Application,2023-01-16,597,300"
     files = {"file": ("additional_fields.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 201, f"Response: {response.text}"
-    assert response.json() == {"detail": "Data uploaded successfully"}
+    assert response.json() == {"detail": ERROR_MESSAGES["upload_successful"]}
+
 
 def test_upload_csv_with_missing_fields():
     csv_data = "review_time,date,merge_time\n0,2023-01-14,0\n0,2023-01-15,0\n715,2023-01-16,597"
     files = {"file": ("missing_fields.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are present in each row."}
+    assert response.json() == {"detail": ERROR_MESSAGES["missing_fields"]}
+
 
 def test_upload_csv_with_invalid_date_format():
     csv_data = "review_time,team,date,merge_time\n0,Application,14/01/2023,0\n0,Application,15/01/2023,0\n715,Application,16/01/2023,597"
     files = {"file": ("invalid_date_format.csv", csv_data, "text/csv")}
     response = client.post("/upload", files=files)
     assert response.status_code == 400, f"Response: {response.text}"
-    assert response.json() == {"detail": "Invalid CSV file. Please make sure all required fields are valid non-negative integers and non-null values."}
+    assert response.json() == {"detail": ERROR_MESSAGES["invalid_date_format"]}
